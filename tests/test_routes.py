c7ea07eab7ea1b4ee1dc02c7df2527e4272ece7b
Delph1 <andreas.galistel@gmail.com>
2023-06-20 12:20:32 -0400

    Added CORS

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -193,3 +193,9 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
+
+    def test_header_access_control_allow_origin(self):
+        """ Checks to see if the Access-Control-Allow-Origin is present"""
+        resp = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(resp.headers.get('Access-Control-Allow-Origin'), '*')
